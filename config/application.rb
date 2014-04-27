c333201e67f13eb39a5f5cbb0759f9c43dcfe37b
Pedro Piñera <pepibumur@gmail.com>
2014-04-27 14:05:49 +0200

    Added tests

--- config/application.rb
+++ config/application.rb
@@ -20,10 +20,10 @@ module Gitbot
     $accounts = YAML.load_file('config/accounts.yml')
     $github = github = Github.new basic_auth: "#{$accounts["github"]["username"]}:#{$accounts["github"]["password"]}"
 
-    # Checking config loaded
+    # Checking config loaded - GITHUG
     require 'github_helper'
     repos_checking_error = Github::Helper.check_repos($accounts["github"]["repos"],$github)
     if repos_checking_error then raise RuntimeError, "#{repos_checking_error}" end
-
+    if !$accounts["github"]["repos"] then raise RuntimeError, "#{repos_checking_error}" end
   end
 end
---
+++ spec/config/application_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+describe Gitbot::Application, 'Application configuration' do
+  context "GITHUB" do
+  	github_account = $accounts["github"]
+  	before(:all) do
+    	github_account["username"] = "pololo"
+    	github_account["password"] = "pilili"
+    	github_account["repos"] = nil
+  	end
+
+  	it 'should exit if no repos setup' do
+		expect{Gitbot::Application}.to raise_error(RuntimeError)
+	end
+
+	it 'should exit if no user or password' do
+		
+	end
+  end
+end
