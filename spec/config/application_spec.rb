160a045f1aa82a89071ed00ba27a4ba43895e916
Pedro Piñera <pepibumur@gmail.com>
2014-05-03 20:54:04 +0200

    Removed invalid test

--- spec/config/application_spec.rb
+++ spec/config/application_spec.rb
@@ -1,27 +1,27 @@
-require "spec_helper"
+# require "spec_helper"
 
-describe Gitbot::Application, 'Application configuration' do
-  context "GITHUB" do
-  	github_account = $accounts["github"]
-  	before(:all) do
-    	github_account["username"] = "pololo"
-    	github_account["password"] = "pilili"
-    	github_account["repos"] = nil
-  	end
+# describe Gitbot::Application, 'Application configuration' do
+#   context "GITHUB" do
+#   	github_account = $accounts["github"]
+#   	before(:all) do
+#     	github_account["username"] = "pololo"
+#     	github_account["password"] = "pilili"
+#     	github_account["repos"] = nil
+#   	end
 
-  	it 'should exit if no repos setup' do
-		expect{Gitbot::Application}.to raise_error(RuntimeError)
-	end
+#   	it 'should exit if no repos setup' do
+# 		expect{Gitbot::Application}.to raise_error(RuntimeError)
+# 	end
 
-	it 'should exit if no user or password' do
+# 	it 'should exit if no user or password' do
 		
-	end
+# 	end
 
 
-	# SHOULD
-	# - Exit if no account
-	# - Exit if no repos
-	# - Exit if no valid account
+# 	# SHOULD
+# 	# - Exit if no account
+# 	# - Exit if no repos
+# 	# - Exit if no valid account
 	
-  end
-end
+#   end
+# end
